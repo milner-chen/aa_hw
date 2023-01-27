2966bd56fa44db106c92979d13907493df4de6ad
Milner Chen <milnerchen0@gmail.com>
2023-01-26 22:09:34 -0500

    Make queue

--- w3d5/stack.rb
+++ w3d5/stack.rb
@@ -1,17 +1,17 @@
 class Stack
-        def initialize
-                @stack = []
-        end
+    def initialize
+        @stack = []
+    end
 
-        def push(el)
-                @stack << el
-        end
+    def push(el)
+        @stack << el
+    end
 
-        def pop
-                @stack.pop
-        end
+    def pop
+        @stack.pop
+    end
 
-        def peek
-                @stack[-1]
-        end
+    def peek
+        @stack[-1]
+    end
 end
