d9183dd6b8159148701af97b78bab89162971192
Milner Chen <milnerchen0@gmail.com>
2023-02-10 00:33:42 -0500

    finish queries

--- w5d4/w5d4_hw/app/models/house.rb
+++ w5d4/w5d4_hw/app/models/house.rb
@@ -1,9 +1,18 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :bigint           not null, primary key
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class House < ApplicationRecord
 
     has_many(
        :residents,
-       class_name: ‘Person’,
-       foreign_key: :professor_id,
+       class_name: 'Person',
+       foreign_key: :house_id,
        primary_key: :id
      )
    
--- w5d4/w5d4_hw/app/models/person.rb
+++ w5d4/w5d4_hw/app/models/person.rb
@@ -1,10 +1,20 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  house_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Person< ApplicationRecord
     validates :name, presence: true
 
     # validates for presence by default
     belongs_to(
         :house,
-        class_name: ‘House’,
+        class_name: 'House',
         foreign_key: :house_id,
         primary_key: :id
       )
