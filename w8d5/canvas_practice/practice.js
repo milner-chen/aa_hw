6a7002f56d0ecefd8c460e735cbe83d10bdc8309
Milner Chen <milnerchen0@gmail.com>
2023-03-03 08:52:28 -0500

    create canvas

--- w8d5/canvas_practice/practice.js
+++ w8d5/canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    let canvas = document.getElementById("mycanvas");
+    canvas.width = 500;
+    canvas.height = 500;
+    let ctx = canvas.getContext('2d');
 });
